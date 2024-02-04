194710eb535a1eb2bb09ffcf7bb5280bddd73eed
Akos Kolloros <kollorosa@gmail.com>
2024-02-04 17:41:47 +0100

    test changes

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import BootstrapVue3 from 'bootstrap-vue-3';
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(BootstrapVue3);
 app.mount('#app')
+console.log("HELLO")
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,13 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <!-- <TheWelcome /> -->
+    <div>
+      <b-button>Button</b-button>
+    </div>
+    <!-- <div>
+      <label for="example-datepicker">Choose a date</label>
+      <b-form-datepicker id="example-datepicker" class="mb-2"></b-form-datepicker>
+      <p>Value: ''</p>
+    </div> -->
   </main>
 </template>
